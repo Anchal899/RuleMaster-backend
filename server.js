8c6d61be1bbefd37a1b35cc93fbaff929524534f
Anchal899 <101783656+Anchal899@users.noreply.github.com>
2024-07-27 19:47:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://rule-master-bice.vercel.app/');
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URL, {
     
 });
 const corsOptions = {
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: 'https://rule-master-bice.vercel.app/', // Allow requests from this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
     credentials: true // Allow credentials (cookies, authorization headers, etc.)

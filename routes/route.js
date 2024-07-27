c9e4dd5ff0798c4fc5dde1d478ffebc7560049d3
Anchal899 <101783656+Anchal899@users.noreply.github.com>
2024-07-27 19:42:33 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const ruleController = require('../controllers/ruleController');
 
+router.get('/',(req,res)=>{
+    res.send('Hello World');
+})
 router.post('/create_rule', ruleController.createRule);
 router.post('/combine_rules', ruleController.combineRules);
 router.post('/evaluate_rule', ruleController.evaluateRule);
